ffe0ffae7f8d34c6acc9f7315d3d6e9564d1c6e7
Himal Edirisinghe <dhke1996@gmail.com>
2025-11-16 09:35:11 +0530

    Added some styles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,18 +4,32 @@ import { Ionicons } from "@expo/vector-icons";
 
 const TabLayout = () => {
     return (
-        <Tabs screenOptions={{}}>
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: "red",
+                tabBarInactiveTintColor: "green",
+                tabBarStyle:{
+                    backgroundColor: "blue",
+                    borderTopWidth: 1,
+                    borderTopColor: "yellow",
+                    height: 90,
+                    paddingBottom: 20,
+                    paddingTop:12,
+                }
+            }}
+        >
             <Tabs.Screen
                 name="index"
                 options={{
                     title: "Todos",
-                    tabBarIcon: ({ color, size }) => <Ionicons name="flash-outline" />,
+                    tabBarIcon: ({ color, size }) => <Ionicons name="flash-outline" size={size} color={color} />,
                 }}
-            />            <Tabs.Screen
+            />
+            <Tabs.Screen
                 name="settings"
                 options={{
                     title: "Settings",
-                    tabBarIcon: ({ color, size }) => <Ionicons name="settings" />,
+                    tabBarIcon: ({ color, size }) => <Ionicons name="settings" size={size} color={color} />,
                 }}
             />
         </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import {StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.content}>Hello World!</Text>
     </View>
   );
 }
